d9308b71d2d64e063d05f5a7caa39f099b1c5c58
sanketpawar1103 <pawarss1103@gmail.com>
2026-01-05 16:10:16 +0530

    Corrected spelling mistakes, and changed some names

--- src/digital_wallet.js
+++ src/digital_wallet.js
@@ -1,9 +1,9 @@
 import { promptSecret } from "@std/cli";
 
-const doesExist = (user, accounts) =>
+const userExistsByPhone = (user, accounts) =>
   accounts.some((each) => each.phone === user.phone);
 
-const updateSheet = (to, from, info) => {
+const applyTransaction = (to, from, info) => {
   to.history.push(
     `${
       new Date().toLocaleString()
@@ -28,10 +28,10 @@ const checkBalance = (user, accounts) => {
     : ` ❌ Invalid Pin ${user.name}\n`;
   console.log(msg);
 
-  return grauntAccess(user, accounts);
+  return grantAccess(user, accounts);
 };
 
-const readDetails = (user) => {
+const readTransactionDetails = (user) => {
   console.log(` ${user.name}\n`);
   const phone = prompt(" Enter receiver's phone number :");
   const amount = parseInt(prompt("Enter amount :"));
@@ -41,7 +41,7 @@ const readDetails = (user) => {
   return { phone, amount, pin };
 };
 
-const areValidDetails = (user, accounts, details) => {
+const areTransactionDetailsValid = (user, accounts, details) => {
   const receiver = accounts.find((each) => each.phone === details.phone);
 
   if (!receiver || receiver.phone === user.phone) {
@@ -58,19 +58,21 @@ const areValidDetails = (user, accounts, details) => {
     return false;
   }
 
-  updateSheet(receiver, user, details);
+  applyTransaction(receiver, user, details);
   return true;
 };
 
 const sendMoney = (user, accounts) => {
-  if (areValidDetails(user, accounts, readDetails(user))) {
-    console.log(` ✅ Transaction Successfull ${user.name}\n`);
+  if (
+    areTransactionDetailsValid(user, accounts, readTransactionDetails(user))
+  ) {
+    console.log(` ✅ Transaction Successful ${user.name}\n`);
   }
 
-  return grauntAccess(user, accounts);
+  return grantAccess(user, accounts);
 };
 
-const getStatements = (user, accounts) => {
+const viewTransactionHistory = (user, accounts) => {
   console.log(` ${user.name}\n`);
   const pin = promptSecret("Enter your pin :");
   console.clear();
@@ -80,7 +82,7 @@ const getStatements = (user, accounts) => {
     : ` ❌ Invalid pin ${user.name}`;
   console.log(msg);
 
-  return grauntAccess(user, accounts);
+  return grantAccess(user, accounts);
 };
 
 const addBalance = (user, accounts) => {
@@ -91,40 +93,40 @@ const addBalance = (user, accounts) => {
 
   if (pin !== user.pin || Number.isNaN(amount) || amount < 0) {
     console.log(` ❌ Invalid pin or amount value ${user.name}`);
-    return grauntAccess(user, accounts);
+    return grantAccess(user, accounts);
   }
 
   console.log(` ✅ Balance added successfully ${user.name}`);
   user.balance += amount;
-  return grauntAccess(user, accounts);
+  return grantAccess(user, accounts);
 };
 
-const exitPage = (_user, accounts) => {
+const logoutAccount = (_user, accounts) => {
   console.clear();
 
   return main(accounts);
 };
 
-const ACTIONS = {
+const userMenuActions = {
   1: checkBalance,
   2: sendMoney,
-  3: getStatements,
+  3: viewTransactionHistory,
   4: addBalance,
-  5: exitPage,
+  5: logoutAccount,
 };
 
-const grauntAccess = (user, accounts) => {
-  let msg = `\n 1. Checke balance\n 2. Send money\n `;
+const grantAccess = (user, accounts) => {
+  let msg = `\n 1. Check balance\n 2. Send money\n `;
   msg += `3. Get statements\n 4. Add balance\n 5. Exit page\n\n`;
   const choice = prompt(msg);
   console.clear();
 
-  if (!(choice in ACTIONS)) {
-    console.log(` ❌ Invalid CHOICE ${user.name}`);
-    return grauntAccess(user, accounts);
+  if (!(choice in userMenuActions)) {
+    console.log(` ❌ Invalid choice ${user.name}`);
+    return grantAccess(user, accounts);
   }
 
-  return ACTIONS[choice](user, accounts);
+  return userMenuActions[choice](user, accounts);
 };
 
 const REGEX = {
@@ -173,7 +175,7 @@ export const isInValidUser = (user, accounts) => {
     return true;
   }
 
-  if (doesExist(user, accounts)) {
+  if (userExistsByPhone(user, accounts)) {
     console.log(" ❌ User already exists\n");
     return true;
   }
@@ -207,17 +209,17 @@ const authenticateUser = (credentials, accounts) =>
     each.phone === credentials.phone && each.pass === credentials.pass
   );
 
-const logIn = (accounts) => {
+const loginUser = (accounts) => {
   const user = authenticateUser(readCredentials(), accounts);
   console.clear();
 
   if (!user) {
-    console.log(" ❌ Credentials missmatch\n");
+    console.log(" ❌ Credentials mismatch\n");
     return main(accounts);
   }
 
   console.log(`\tWelcome ${user.name}`);
-  return grauntAccess(user, accounts);
+  return grantAccess(user, accounts);
 };
 
 const createAccount = (accounts) => {
@@ -226,34 +228,36 @@ const createAccount = (accounts) => {
     return main(accounts);
   }
 
-  console.log(" ✅ Account created successfylly\n");
+  console.log(" ✅ Account created successfully\n");
   accounts.push(user);
 
   return main(accounts);
 };
 
-const exitHome = (accounts) =>
+const saveAndExit = (accounts) =>
   Deno.writeTextFileSync(
     "./accounts.json",
     JSON.stringify(accounts, null, 2),
   );
 
-const appActions = {
+const homeMenuActions = {
   1: createAccount,
-  2: logIn,
-  3: exitHome,
+  2: loginUser,
+  3: saveAndExit,
 };
 
 const main = (accounts) => {
-  const choice = prompt(" 1. Create Account\n 2. Log In\n 3. Exit\n\n");
+  const choice = prompt(
+    " 1. Create Account\n 2. Log In\n 3. Close application\n\n",
+  );
 
-  if (!(choice in appActions)) {
+  if (!(choice in homeMenuActions)) {
     console.clear();
-    console.log(" ❌ Invalid CHOICE\n");
+    console.log(" ❌ Invalid choice\n");
     return main(accounts);
   }
 
-  return appActions[choice](accounts);
+  return homeMenuActions[choice](accounts);
 };
 
 const fetchAllAccounts = () => {
